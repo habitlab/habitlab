8acc6777a9e8bb1de77f1ef040ac310ff48dcf4f
Mathieu Rolfo <rolfom01@gmail.com>
2016-07-07 11:35:19 -0700

    Added interstitial screen code and modified interventions list.

--- src/interventions/facebook/show_user_info_interstitial/frontend.js
+++ src/interventions/facebook/show_user_info_interstitial/frontend.js
@@ -1,3 +1,27 @@
 (() => {
+  if (window.show_user_info_interstitial) {
+    return;
+  }
+  window.show_user_info_interstitial = true;
+
+  const $ = require('jquery');
+  require('enable-webcomponents-in-content-scripts');
+  require('components/interstitial-screen-polymer.deps');
+
+  const {
+    get_minutes_spent_on_domain_today
+  } = require('libs_common/time_spent_utils');
+
+  const {
+    log_impression,
+    log_action
+  } = require('libs_common/log_utils');
+
+
+  var interstitial = $('<interstitial-screen-polymer>');
+  console.log(interstitial);
+  console.log('loading interstitial');
+  $(document.body).append(interstitial);
+
 
 })()

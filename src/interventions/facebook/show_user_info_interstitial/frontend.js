2f45025cdd81086ae29599949d1e3bfee9635c12
Mathieu Rolfo <rolfom01@gmail.com>
2016-07-08 11:32:50 -0700

    Fixed interstitial page displaying information

--- src/interventions/facebook/show_user_info_interstitial/frontend.js
+++ src/interventions/facebook/show_user_info_interstitial/frontend.js
@@ -24,18 +24,21 @@
   } = require('libs_common/domain_utils')
 
 
-  
-
-
-  var numMinutesSpent = 2;
   get_minutes_spent_on_domain_today(url_to_domain(window.location.href), function(numMins){
     
     get_visits_to_domain_today(url_to_domain(window.location.href), function(numVisits) {
       
+
+
+      console.log(numVisits)
       var titleString = 'You have visited ' + url_to_domain(window.location.href) +' ' + numVisits + ' times and spent '+ numMins + ' minutes there today.'
       titleString += ' \n Take this chance to do something more valuable with your time!'
-      var interst_screen = $('<interstitial-screen-polymer btn-txt="Click to continue to Facebook">')
-      interst_screen.prop('titleText', titleString)
+      var buttonText = 'Click to continue to Facebook'
+      console.log(buttonText)
+      var interst_screen = $('<interstitial-screen-polymer btn-txt="hollla">')
+      interst_screen.attr('btn-txt', buttonText)
+      interst_screen.attr('title-text', titleString)
+      interst_screen.attr('intervention', 'facebook/show_user_info_interstitial')
       $(document.body).append(interst_screen)
     })
   });

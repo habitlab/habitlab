35d084dd3a0ee2d8cdbb4f1f4edc87800a942f3a
Sarah Sukardi <sukardi@jhu.edu>
2016-07-12 15:52:13 -0700

    added close tab button to polymer component

--- src/components/interstitial-screen-polymer.js
+++ src/components/interstitial-screen-polymer.js
@@ -1,9 +1,7 @@
 const $ = require('jquery')
 
-
 const {polymer_ext} = require('libs_frontend/polymer_utils')
-
-
+const {close_selected_tab} = require('libs_frontend/tab_utils')
 
 Polymer({
   is: 'interstitial-screen-polymer',
@@ -12,6 +10,9 @@ Polymer({
     btnTxt: {
       type: String, 
     },
+    btnTxt2: {
+      type: String, 
+    },    
     minutes: {
       type: Number
     },
@@ -35,19 +36,27 @@ Polymer({
     'show_button': 'showButton'
   },
   buttonclicked: function() {
-    console.log('ok button clicked in polymer during loadin')
+    console.log('ok button clicked in polymer during loading')
     $(this).hide()
+  },
+  buttonclicked2: function() {
+    close_selected_tab().then(() => {
+      console.log('done closing tab')
+    });
   },
   hideButton: function() {
     this.$.okbutton.hidden = true
+    this.$.closetabbutton.hidden = true
   },
   showButton: function() {
     console.log(this.$.okbutton)
     this.$.okbutton.hidden = false
+    this.$.closetabbutton.hidden = false
   },
   ready: function() {
     console.log('interstitial-polymer ready')
     this.$.okbutton.textContent = this.btnTxt
+    this.$.closetabbutton.textContent = this.btnTxt2
     this.$.titletext.textContent = this.titleText
     this.$.messagetext.textContent = this.messageText
     console.log(this.$.titletext.textContent)
@@ -63,6 +72,7 @@ Polymer({
   
   attributeChanged: function() {
     this.$.okbutton.textContent = this.btnTxt 
+    this.$.closetabbutton.textContent = this.btnTxt2
     this.$.messagetext.textContent = this.messageText
     this.$.titletext.textContent = this.titleText
     console.log(this.$.titletext.textContent)

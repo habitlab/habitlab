f79a274254f0e8f438266bb249fc809f35d19aad
Geza Kovacs <gkovacs@stanford.edu>
2016-09-28 17:36:42 -0700

    green background intervention switch to using once_available function

--- src/interventions/reddit/green_background/frontend.js
+++ src/interventions/reddit/green_background/frontend.js
@@ -1,5 +1,15 @@
 const $ = require('jquery')
 
-setTimeout(() => {
+const {
+  once_available
+} = require('libs_frontend/common_libs')
+
+// const once_available = require('libs_frontend/common_libs')['once_available']
+
+//setTimeout(() => {
+//  $('body').css('background-color', 'green')
+//}, 3000)
+
+once_available('body', () => {
   $('body').css('background-color', 'green')
-}, 3000)
+})

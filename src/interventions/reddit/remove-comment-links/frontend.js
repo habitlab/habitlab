762d387633505b499ea5187afc6c7a80927802d1
brahmcapoor <brahmcapoor@gmail.com>
2016-10-03 14:34:00 -0700

    Added new intervention - remove links to comment sections on reddit

---
+++ src/interventions/reddit/remove-comment-links/frontend.js
@@ -0,0 +1,10 @@
+const $ = require('jquery')
+
+const {
+  once_available
+} = require('libs_frontend/common_libs')
+
+once_available('body', () => {
+  $('.first').remove();
+  alert("Habitlab has removed links to the comment sections. Don't go down the rabbit hole!");
+});

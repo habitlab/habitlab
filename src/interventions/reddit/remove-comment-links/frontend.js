edbea20062b163092c0317ac162f0fe7ae525cf6
brahmcapoor <brahmcapoor@gmail.com>
2016-10-05 18:37:55 -0700

    Switched reddit comment intervention to sweetalert

--- src/interventions/reddit/remove-comment-links/frontend.js
+++ src/interventions/reddit/remove-comment-links/frontend.js
@@ -4,8 +4,19 @@ const {
   once_available
 } = require('libs_frontend/common_libs')
 
-once_available('body', () => {
-  $('.first').remove();
-  $('.redditSingleClick').remove();
-  alert("Habitlab has removed links to the comment sections. Don't go down the rabbit hole!");
-});
+const swal = require('sweetalert2')
+
+const {
+  load_css_file
+} = require('libs_common/content_script_utils')
+
+const co = require('co')
+
+co(function*() {
+  yield load_css_file('bower_components/sweetalert2/dist/sweetalert2.css');
+  once_available('body', () => {
+    $('.first').remove();
+    $('.redditSingleClick').remove();
+    swal("Habitlab has removed links to the comment sections. Don't go down the rabbit hole!");
+  });
+})

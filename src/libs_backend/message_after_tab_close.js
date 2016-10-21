67cb7ae2673320bbd496458554da5b5db14a806a
Geza Kovacs <gkovacs@stanford.edu>
2016-10-21 16:30:40 -0700

    added example of using chrome.tabs.executeScript

--- src/libs_backend/message_after_tab_close.js
+++ src/libs_backend/message_after_tab_close.js
@@ -1,3 +1,9 @@
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
   console.log('tab was removed ' + tabId)
+  // to change the background color of the page
+  /*
+  setTimeout(function() {
+    chrome.tabs.executeScript({code: 'document.querySelector("body").style.backgroundColor = "green"'})
+  }, 3000)
+  */
 })

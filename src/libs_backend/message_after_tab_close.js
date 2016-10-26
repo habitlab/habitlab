2a5725c4f3ec09ec2c2e0b97dd60d087706289a9
brahmcapoor <brahmcapoor@gmail.com>
2016-10-26 16:08:52 -0700

    Added frontend utils for tab close message

--- src/libs_backend/message_after_tab_close.js
+++ src/libs_backend/message_after_tab_close.js
@@ -1,9 +1,11 @@
+//const {polymer_ext} = require('libs_frontend/polymer_utils');
+
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-  console.log('tab was removed ' + tabId)
+  console.log('tab with id ' + tabId + ' was closed')
   // to change the background color of the page
-  /*
   setTimeout(function() {
-    chrome.tabs.executeScript({code: 'document.querySelector("body").style.backgroundColor = "green"'})
+    //chrome.tabs.executeScript({code: 'var notif=document.createElement("paper-scroll-header-panel");notif.text_content="You just closed a tab";'})
+    chrome.tabs.executeScript({file: 'frontend_utils/close_tab_message.js'});
   }, 3000)
-  */
+
 })

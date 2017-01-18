187d0a5ca10b713f47fd024e16ef112db0dd039e
Geza Kovacs <gkovacs@stanford.edu>
2017-01-18 15:43:47 -0800

    added feedback-button-right to options-view

--- src/components/feedback-button-right.js
+++ src/components/feedback-button-right.js
@@ -2,37 +2,9 @@ const {polymer_ext} = require('libs_frontend/polymer_utils');
 
 polymer_ext({
   is: 'feedback-button-right',
-  properties: {
-    width: {
-      type: String,
-      value: '38px',
-    },
-    height: {
-      type: String,
-      value: '38px',
-    },
-    unclickable: {
-      type: Boolean,
-      value: false
-    },
-  },
-  get_img_style: function() {
-    return `width: ${this.width}; height: ${this.height};`
-  },
-  ready: function() {
-    if (this.unclickable) {
-      this.style.cursor = "default";
-    }
-  },
-  habitlab_button_clicked: function() {
-    //const habitlab_options_popup = document.createElement('habitlab-options-popup')
-    //document.body.appendChild(habitlab_options_popup)
-    //habitlab_options_popup.open()
+  feedback_button_clicked: function() {
     System.import('bugmuncher/bugmuncher').then(function() {
       window.open_bugmuncher()
     })
-  },
-  get_url: function() {
-    return chrome.extension.getURL('icons/feedback_icon.svg');
   },
 });
--- src/components/feedback-button.js
+++ src/components/feedback-button.js
@@ -11,23 +11,11 @@ polymer_ext({
       type: String,
       value: '38px',
     },
-    unclickable: {
-      type: Boolean,
-      value: false
-    },
   },
   get_img_style: function() {
     return `width: ${this.width}; height: ${this.height};`
   },
-  ready: function() {
-    if (this.unclickable) {
-      this.style.cursor = "default";
-    }
-  },
-  habitlab_button_clicked: function() {
-    //const habitlab_options_popup = document.createElement('habitlab-options-popup')
-    //document.body.appendChild(habitlab_options_popup)
-    //habitlab_options_popup.open()
+  feedback_button_clicked: function() {
     System.import('bugmuncher/bugmuncher').then(function() {
       window.open_bugmuncher()
     })

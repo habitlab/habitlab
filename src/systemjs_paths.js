998557465df3914560a47eba9ab1e32b71a69f2e
Geza Kovacs <gkovacs@stanford.edu>
2017-01-29 01:33:23 -0800

    fix issue with missing js defaultExtension in systemjs 0.20

--- src/systemjs_paths.js
+++ src/systemjs_paths.js
@@ -1,9 +1,28 @@
 SystemJS.config({
+  defaultJSExtensions: true,
   baseURL: chrome.extension.getURL('/'),
   packages: {
-    '.': {}
+    'libs_frontend': {
+      defaultExtension: 'js',
+    },
+    'libs_backend': {
+      defaultExtension: 'js',
+    },
+    'libs_common': {
+      defaultExtension: 'js',
+    },
+    'generated_libs/libs_frontend': {
+      defaultExtension: 'js',
+    },
+    'generated_libs/libs_backend': {
+      defaultExtension: 'js',
+    },
+    'components': {
+      defaultExtension: 'js',
+    },
   },
   browserConfig: {
+    defaultJSExtensions: true,
     baseURL: chrome.extension.getURL('/'),
     paths: {
       'npm:': chrome.extension.getURL("/jspm_packages/npm/"),

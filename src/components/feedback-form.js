5cc10ba8f2df70f618199b1ff8cf8d2d0135cd87
Geza Kovacs <gkovacs@stanford.edu>
2017-02-05 23:40:38 -0800

    fixed scrolling in feedback-form

--- src/components/feedback-form.js
+++ src/components/feedback-form.js
@@ -1,5 +1,6 @@
 const {polymer_ext} = require('libs_frontend/polymer_utils');
 const {cfy} = require('cfy');
+const _ = require('underscore');
 
 polymer_ext({
   is: 'feedback-form',
@@ -23,6 +24,14 @@ polymer_ext({
     mailing_list: {
       type: String,
       value: [['habitlab', 'support'].join('-'), ['cs', 'stanford', 'edu'].join('.')].join('@')
+    },
+    feedback: {
+      type: String,
+      value: ''
+    },
+    email: {
+      type: String,
+      value: ''
     }
   },
   isdemo_changed: function() {
@@ -30,6 +39,9 @@ polymer_ext({
       this.open();
     }
   },
+  textarea_changed: _.throttle(function() {
+    this.$$('#feedback_dialog').notifyResize();
+  }, 300),
   feedback_button_clicked: cfy(function*() {
     var screenshot_utils = yield SystemJS.import('libs_common/screenshot_utils');
     var screenshot = yield screenshot_utils.get_screenshot_as_base64();

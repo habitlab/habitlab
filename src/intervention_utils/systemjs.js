7d8abea6dce9b6f5c6ac2d636e8913d8a6830e23
Geza Kovacs <gkovacs@stanford.edu>
2017-02-16 17:48:52 -0800

    version 1.0.78

--- src/intervention_utils/systemjs.js
+++ src/intervention_utils/systemjs.js
@@ -1,1 +1,18 @@
+(function() {
+  var {localget} = require('libs_frontend/cacheget_utils');
+  if (!window.fetch.overridden) {
+    var fetch_orig = window.fetch;
+    window.fetch_orig = fetch_orig;
+    window.fetch = function(input, init) {
+      if (input.startsWith('chrome-extension://')) {
+        return localget(input).then(function(text) {
+          return new Response(text);
+        });
+      }
+      return fetch_orig(input, init);
+    }
+    window.fetch.overridden = true;
+  }
+})();
+
 require('libs_common/systemjs');

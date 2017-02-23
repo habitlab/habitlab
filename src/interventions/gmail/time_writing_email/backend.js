eb5de728440efddfdd44075dfd775e32ad897a82
Swathi Iyer <swathii@stanford.edu>
2017-02-22 19:17:01 -0800

    added intervention gmail/time_writing_email

---
+++ src/interventions/gmail/time_writing_email/backend.js
@@ -0,0 +1,69 @@
+const {
+  log_impression,
+  log_action,
+} = require('libs_common/log_utils')
+
+var myNotifId = null;
+
+//Displays a rich notification to the user
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  
+  if (request.type === "chrome-notification") {
+    chrome.notifications.create('chrome-notification', {
+      type: 'basic',
+      iconUrl: chrome.extension.getURL("interventions/gmail/time_writing_email/rich_notif_icon.png"),
+      title: "Whoa there!",
+      message: "You've already spent " + request.timeSpent + " minutes composing this email.",
+      requireInteraction: false,
+      buttons: [
+        { title: 'Close Gmail', iconUrl: chrome.extension.getURL('interventions/gmail/time_writing_email/close_tab.png')},
+        { title: 'Dismiss', iconUrl: chrome.extension.getURL('interventions/gmail/time_writing_email/dismiss.png')}
+      ]
+    }, function callback(id) {      
+      myNotifId = id;
+    });
+  }
+
+});
+
+/*
+chrome.runtime.onButtonClicked.addListener(function(message, sender, sendResponse) {
+  var b = document.getElementsByClassName('T-I J-J5-Ji T-I-KE L3');
+  b.addEventListener('click', function() {  
+  });    
+});
+*/
+
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+  if (notifId === myNotifId) {
+    if (btnIdx === 0) {
+      //Log the action: the user closed the facebook tab
+      log_action('gmail/time_writing_email', {'positive': 'User closed Facebook.'})
+      closeTab();
+      closeNotif(notifId);      
+    } else if (btnIdx === 1) {
+      //Log the action: the user did not close the facebook tab
+      log_action('gmail/time_writing_email', {'negative': 'User remained on Facebook'});
+      closeNotif(notifId);
+    }
+  } 
+  
+});
+
+function closeTab() {
+  chrome.tabs.query({active: true, windowId: chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
+    for (let tab of tabs) {
+      chrome.tabs.remove(tab.id)
+    }
+  })
+}
+
+function closeNotif(notifId) {
+  chrome.notifications.clear(notifId, function callback(wasCleared) {        
+  });  
+}
+
+
+
+window.debugeval = x => eval(x);
+
---
+++ src/interventions/gmail/time_writing_email/frontend.js
@@ -0,0 +1,100 @@
+const $ = require('jquery')
+
+const {
+  get_seconds_spent_on_current_domain_today,
+  get_seconds_spent_on_domain_today,
+} = require('libs_common/time_spent_utils')
+
+const {
+  printable_time_spent,
+} = require('libs_common/time_utils')
+
+const {
+  log_impression,
+  log_action,
+} = require('libs_common/log_utils')
+
+
+function shouldInsert(secondsSpent, timeInterval) {
+  if (secondsSpent < timeInterval * 60) {
+    return false; // only show after a minute
+  }
+  const newestInterval = Math.floor(Math.floor(secondsSpent/60) / timeInterval)
+
+  if (!localStorage.hasOwnProperty("currInterval")) {
+    localStorage.currInterval = newestInterval
+    return true
+  } else { //property already exists in storage
+    //A new day
+    if (newestInterval < parseInt(localStorage.currInterval, 10)) {  
+      localStorage.currInterval = newestInterval
+      return true
+    } else if (newestInterval === parseInt(localStorage.currInterval, 10)) {
+      return false
+    } else { //newestInterval > localStorage.currInterval
+      localStorage.currInterval = newestInterval;
+      return true
+    }
+  }
+}
+
+function insertRichNotification() {  
+  get_seconds_spent_on_current_domain_today(function(secondsSpent) {
+    console.log(secondsSpent);
+    //if (shouldInsert(secondsSpent, intervention.params.minutes.value)) {
+      log_impression('facebook/rich_notifications')
+      chrome.runtime.sendMessage({type: "chrome-notification", timeSpent: printable_time_spent(secondsSpent)}, (response) => {});
+    //}
+  })
+}
+
+function insert_composing_time_notification(secondsSpent) {
+  chrome.runtime.sendMessage({type: "chrome-notification", timeSpent: printable_time_spent(secondsSpent)}, (response) => {});
+}
+
+/*
+var seconds_spent_composing_email = 0;
+setInterval(function() {
+  if (window.location.href.includes('?compose=new')) {
+    ++seconds_spent_composing_email;
+    console.log(seconds_spent_composing_email);
+    if (shouldInsert(seconds_spent_composing_email, intervention.params.minutes.value)) {
+      console.log('displaying notification');
+      insert_composing_time_notification(seconds_spent_composing_email);
+    }
+  }
+}, 1000);
+*/
+
+setInterval(function() {
+  var compose_window = $('.nH.Hd');
+  if (window.location.href.includes('?compose=new')) {
+    var seconds_spent_composing_email = compose_window.data('time_open');
+    if (seconds_spent_composing_email == null) {
+      seconds_spent_composing_email = 0;
+      compose_window.data('time_open', 0);
+    }
+    ++seconds_spent_composing_email;
+    $('.nH.Hd').data('time_open', seconds_spent_composing_email);
+    if (shouldInsert(seconds_spent_composing_email, intervention.params.minutes.value)) {
+      console.log('displaying notification');
+      insert_composing_time_notification(seconds_spent_composing_email);
+    }
+  }
+}, 1000)
+
+function addComposeButtonListener() {
+  var compose_button=$('.T-I.J-J5-Ji.T-I-KE.L3');
+  compose_button.click(function() {
+    alert('foobar')
+  })
+}
+
+function main() {
+  insertRichNotification();
+  addComposeButtonListener();
+}
+
+//$(document).ready(main);
+
+window.debugeval = x => eval(x);

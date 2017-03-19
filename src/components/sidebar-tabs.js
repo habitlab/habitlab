b45b3655066c221e73a188e1286a6351426bd760
Geza Kovacs <gkovacs@stanford.edu>
2017-03-19 16:50:19 -0700

    added feedback button on sidebar-tabs

--- src/components/sidebar-tabs.js
+++ src/components/sidebar-tabs.js
@@ -1,4 +1,6 @@
 const {polymer_ext} = require('libs_frontend/polymer_utils');
+const {cfy} = require('cfy');
+const screenshot_utils = require('libs_common/screenshot_utils');
 
 polymer_ext({
 
@@ -14,6 +16,10 @@ polymer_ext({
     isdemo: {
       type: Boolean,
       observer: 'isdemo_changed'
+    },
+    feedback_icon: {
+      type: String,
+      value: chrome.extension.getURL('icons/feedback_icon.svg')
     }
   },
   isdemo_changed: function(isdemo) {
@@ -24,7 +30,23 @@ polymer_ext({
   },
   tab_elem_selected: function(evt) {
     this.selected_tab_idx = evt.target.idx;
-  }
+  },
+  feedback_button_clicked: cfy(function*() {
+    //var screenshot_utils = yield SystemJS.import('libs_common/screenshot_utils');
+    var screenshot = yield screenshot_utils.get_screenshot_as_base64();
+    var data = yield screenshot_utils.get_data_for_feedback();
+    var feedback_form = document.createElement('feedback-form')
+    document.body.appendChild(feedback_form);
+    feedback_form.screenshot = screenshot;
+    feedback_form.other = data;
+    feedback_form.open();
+    
+    /*
+    SystemJS.import('bugmuncher/bugmuncher').then(function() {
+      window.open_bugmuncher()
+    })
+    */
+  })
 }, {
   source: require('libs_frontend/polymer_methods'),
   methods: [

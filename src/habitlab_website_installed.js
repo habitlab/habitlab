4a5ecf8a0e07333395b668332f93aeef56f353ab
Geza Kovacs <gkovacs@stanford.edu>
2017-03-19 21:20:10 -0700

    content script injects thing into page

---
+++ src/habitlab_website_installed.js
@@ -0,0 +1,6 @@
+var marker_div = document.createElement('div');
+marker_div.setAttribute('id', 'habitlab_already_installed_marker');
+
+window.onload = function() {
+  document.body.appendChild(marker_div);
+}

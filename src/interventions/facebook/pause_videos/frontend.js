f3cf11bb55397be91455ddf87197f02fe2b2f172
Geza Kovacs <gkovacs@stanford.edu>
2017-05-02 23:18:36 -0700

    prevent the facebook pause videos intervention from breaking videos. fixes #257

--- src/interventions/facebook/pause_videos/frontend.js
+++ src/interventions/facebook/pause_videos/frontend.js
@@ -5,10 +5,35 @@ const $ = require('jquery')
 
 var video_pauser = null;
 
+let video_locations = [];
+
+$(document).click((evt) => {
+  const {offsetX, offsetY} = evt;
+  for (let {left, top, width, height, video} of video_locations) {
+    if (left <= offsetX <= left+width && top <= offsetY <= top+height) {
+      $(video).attr('pauser_disabled', 'true')
+    }
+  }
+})
+
 function pauseAll() {
   video_pauser = setInterval(function() {
+    video_locations = [];
     for (let x of $('video')) {
-      x.pause();
+      const $x = $(x)
+      const {top, left} = $x.offset()
+      const width = $x.width()
+      const height = $x.height()
+      video_locations.push({
+        left,
+        top,
+        width,
+        height,
+        video: x,
+      })
+      if (!$x.attr('pauser_disabled')) {
+        x.pause();
+      }
     }
   }, 1000)
 }

15eefafe05bc174b09e982d18346ec26859a5f68
Geza Kovacs <geza0kovacs@gmail.com>
2017-07-24 15:46:30 -0700

    added new generic intervention show_timer_banner

--- src/components/show-timer-banner.js
+++ src/components/show-timer-banner.js
@@ -8,9 +8,30 @@ const {
   get_seconds_spent_on_current_domain_today
 } = require('libs_common/time_spent_utils');
 
+const {
+  get_intervention
+} = require('libs_common/intervention_info')
+
 Polymer({
   is: 'show-timer-banner',
+  properties: {
+    duration: {
+      type: Number,
+      value: 0
+    },
+    sitename_printable: {
+      type: String,
+      value: (function() {
+        let intervention_info = get_intervention()
+        if (intervention_info.sitename_printable) {
+          return intervention_info.sitename_printable
+        }
+        return 'this site'
+      })()
+    }
+  },
   ready: function() {
+    console.log('show-timer-banner ready')
     let self = this
     let is_shown = true
     let update_time = function() {
@@ -21,11 +42,11 @@ Polymer({
     update_time()
     setInterval(update_time, 1000)
     $(document).mousemove(function(evt) {
-      let messagediv_offset = $(self.$.messagediv).offset()
+      let messagediv_offset = $(self.$.showtimerbannermessagediv).offset()
       let min_x = messagediv_offset.left
-      let min_y = messagediv_offset.top
-      let max_x = min_x + $(self.$.messagediv).width()
-      let max_y = min_y + $(self.$.messagediv).height()
+      let min_y = 0 // messagediv_offset.top
+      let max_x = min_x + $(self.$.showtimerbannermessagediv).width()
+      let max_y = min_y + $(self.$.showtimerbannermessagediv).height()
       let x = evt.clientX
       let y = evt.clientY
       let prev_is_shown = is_shown
@@ -36,11 +57,11 @@ Polymer({
       }
       if (prev_is_shown != is_shown) {
         if (is_shown) {
-          $(self.$.messagediv).css({
+          $(self.$.showtimerbannermessagediv).css({
             opacity: 1
           })
         } else {
-          $(self.$.messagediv).css({
+          $(self.$.showtimerbannermessagediv).css({
             opacity: 0
           })
         }
--- src/interventions/generic/show_timer_banner/frontend.js
+++ src/interventions/generic/show_timer_banner/frontend.js
@@ -1,2 +1,24 @@
-alert('show_timer_banner running')
+window.Polymer = window.Polymer || {}
+window.Polymer.dom = 'shadow'
+if (typeof(window.wrap) != 'function') {
+  window.wrap = null
+}
 
+let {
+  append_to_body_shadow
+} = require('libs_frontend/common_libs')
+
+let $ = require('jquery')
+
+require('enable-webcomponents-in-content-scripts')
+
+require('components/show-timer-banner.deps')
+
+let scroll_block_display = $('<show-timer-banner>');
+let shadow_div = append_to_body_shadow(scroll_block_display);
+
+window.on_intervention_disabled = function() {
+  $(shadow_div).remove()
+}
+
+window.debugeval = x => eval(x)

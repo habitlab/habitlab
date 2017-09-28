de0b072039a6581458922ba5603043667ea4fb1c
Geza Kovacs <geza0kovacs@gmail.com>
2017-09-28 12:32:21 -0700

    updated remove_sidebar_links intervention to work on new youtube

--- src/interventions/youtube/remove_sidebar_links/frontend.js
+++ src/interventions/youtube/remove_sidebar_links/frontend.js
@@ -18,17 +18,52 @@ const removeSidebarOnceAvailable = run_only_one_at_a_time((callback) => {
     return
   }
   once_available_fast('.watch-sidebar-section', () => {
-    removeSidebar('#watch7-sidebar-contents')
+    // old youtube
+    removeSidebarOld('#watch7-sidebar-contents')
     callback()
   })
-  once_available_fast('#items', () => {
-    removeSidebar('#items')
+  once_available_fast('#related', () => {
+    removeSidebar('#related')
     callback()
   })
 })
 
 //Nukes links on the sidebar
 function removeSidebar(sidebar_selector) {
+  console.log('new youtube');
+  if (window.intervention_disabled) {
+    return
+  }
+  if ($('.habitlab_inserted_div').length > 0) {
+    return
+  }
+	//remove the links on the sidebar
+	/*
+  const sidebarLink = document.querySelectorAll('.watch-sidebar-section');
+	for (let link of sidebarLink) {
+		link.parentNode.removeChild(link)
+	}
+  */
+  for (let sidebar of $(sidebar_selector)) {
+    for (let child of $(sidebar).children()) {
+      $(child).css({display: 'none', opacity: 0})
+    }
+  }
+  let habitlab_inserted_div = $('<div style="width: 100%; text-align: center">')
+  habitlab_inserted_div.append($('<habitlab-logo-v2>'))
+  habitlab_inserted_div.append($('<br>'))
+  let show_sidebar_button = $('<paper-button style="background-color: #415D67; color: white; -webkit-font-smoothing: antialiased; font-size: 14px; box-shadow: 2px 2px 2px #888888; margin-top: 10px">Show Sidebar</paper-button>')
+  show_sidebar_button.click(function() {
+    disable_intervention()
+  })
+  show_sidebar_button.appendTo(habitlab_inserted_div)
+  let habitlab_inserted_div_wrapper = $(wrap_in_shadow(habitlab_inserted_div)).addClass('habitlab_inserted_div')
+  $(sidebar_selector).prepend(habitlab_inserted_div_wrapper)
+}
+
+//Nukes links on the sidebar
+function removeSidebarOld(sidebar_selector) {
+  console.log('old youtube');
   if (window.intervention_disabled) {
     return
   }
@@ -68,6 +103,11 @@ on_url_change(() => {
 function disable_intervention() {
   $('.habitlab_inserted_div').remove()
   for (let sidebar of $('#watch7-sidebar-contents')) {
+    for (let child of $(sidebar).children()) {
+      $(child).css({display: 'block', opacity: 1})
+    }
+  }
+  for (let sidebar of $('#related')) {
     for (let child of $(sidebar).children()) {
       $(child).css({display: 'block', opacity: 1})
     }

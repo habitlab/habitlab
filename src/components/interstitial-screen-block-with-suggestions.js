9841fd7caa62b8f3f7e0e0d9e305027edc451b5a
britnichau <30879754+britnichau@users.noreply.github.com>
2017-10-11 17:34:01 -0700

    added site blocker and site blocker with suggestions interventions

---
+++ src/components/interstitial-screen-block-with-suggestions.js
@@ -0,0 +1,150 @@
+const $ = require('jquery')
+
+const {polymer_ext} = require('libs_frontend/polymer_utils')
+const {close_selected_tab} = require('libs_frontend/tab_utils')
+
+const {
+  log_action,
+} = require('libs_frontend/intervention_log_utils')
+
+const {
+  get_positive_enabled_goals
+} = require('libs_common/goal_utils')
+
+Polymer({
+  is: 'interstitial-screen-block-with-suggestions',
+  doc: 'A screen that either shows work sites visited today or suggested links to New York Times articles.',
+  properties: {
+    positive_goals: {
+      type: Array,
+      value: []
+    }
+  },
+  ready: async function() {
+    let positive_enabled_goals = await get_positive_enabled_goals()
+    let positive_goals = []
+    for (let goal_name of Object.keys(positive_enabled_goals)) {
+      let goal_info = positive_enabled_goals[goal_name]
+      positive_goals.push(goal_info)
+    }
+    this.positive_goals = positive_goals
+  }
+  /*
+  properties: {
+    btnTxt: {
+      type: String, 
+    },
+    btnTxt2: {
+      type: String, 
+    },    
+    minutes: {
+      type: Number
+    },
+    titleText: {
+      type: String, 
+    },
+    visits: {
+      type: Number
+    },
+    intervention: {
+      type: String
+    },
+    messageText: {
+      type: String
+    },
+    randomizer: {
+      type: Boolean,
+      value: Math.floor(Math.random()*2) == 0,
+    },
+    show_rss_message: {
+      type: Boolean,
+      computed: 'compute_show_rss_message(show_progress_message, randomizer)',
+    },
+    show_workpages_message: {
+      type: Boolean,
+      computed: 'compute_show_workpages_message(show_progress_message, randomizer)',
+    },
+    show_progress_message: {
+      type: Boolean,
+      value: false,
+      //computed: 'compute_progress_message()',
+    }
+  },
+
+  listeners: {
+    'disable_intervention': 'disableIntervention',
+    'show_button': 'showButton'
+  },
+  compute_show_rss_message: function(show_progress_message, randomizer) {
+    return (!show_progress_message) && randomizer
+  },
+  compute_show_workpages_message: function(show_progress_message, randomizer) {
+    return !show_progress_message && !randomizer
+  },
+  //compute_show_progress_message: function() {
+  //  return false
+  //},
+  buttonclicked: function() {
+    log_action({'negative': 'Continuted to site.'})
+    $(this).hide()
+  },
+  hideButton: function() {
+    //$(this.$.okbutton).fadeOut("slow")
+    this.$.okbutton.hidden = true
+    //this.$.closetabbutton.hidden = true
+    this.$.okbutton.style.display = 'none';
+    //this.$.closetabbutton.style.display = 'none';
+  },
+  showProgress: function() {
+    this.$.paperprogress.style.display = 'block';
+  },
+  incrementProgress: function() {
+    this.$.paperprogress.value += 1;
+  },
+  setProgress: function(val) {
+    this.$.paperprogress.value = val;
+  },
+  showButton: function() {
+    console.log(this.$.okbutton)
+    $(this.$.okbutton).fadeIn("slow")
+    this.$.okbutton.hidden = false
+    // this.$.closetabbutton.hidden = false
+    this.$.okbutton.style.display = 'inline-flex';
+    // this.$.closetabbutton.style.display = 'inline-flex';
+  },
+  ready: function() {
+    this.$.okbutton.textContent = this.btnTxt
+    this.$.closetabbutton.text = this.btnTxt2
+    this.$.titletext.textContent = this.titleText
+    this.$.messagetext.textContent = this.messageText
+    console.log(this.$.titletext.textContent)
+    this.addEventListener('show_button', function() {
+      console.log('hi')
+    })
+    
+    //this.$.titletext.hidden = true
+    // this.$.closetabbutton.hidden = true
+    // var self = this
+    // $(this.$.titletext).fadeIn().promise().then(function() {
+    //   console.log('faded in title-text!')
+    //   $(self.$.closetabbutton).fadeIn(5000, function() {
+    //     console.log('faded in close-tab')
+    //   })
+    // })
+
+    // this.$.paperprogress.hidden = true
+    // this.$.paperprogress.style.display = 'none';
+  },
+  disableIntervention: function() {
+    $(this).hide()
+  },
+  
+  attributeChanged: function() {
+    this.$.okbutton.textContent = this.btnTxt 
+    this.$.closetabbutton.text = this.btnTxt2
+    this.$.messagetext.textContent = this.messageText
+    this.$.titletext.textContent = this.titleText
+  }
+  */
+});
+
---
+++ src/interventions/generic/site_blocker_with_suggestions/frontend.js
@@ -0,0 +1,75 @@
+/*
+alert('foobar');
+console.log('site blocker intervention running');
+*/
+
+/*
+console.log('site blocker running')
+
+const {
+  get_positive_enabled_goals
+} = require('libs_common/goal_utils')
+
+async function main() {
+  let positive_enabled_goals = await get_positive_enabled_goals()
+  console.log(positive_enabled_goals)
+  for (let goal_name of Object.keys(positive_enabled_goals)) {
+    let goal_info = positive_enabled_goals[goal_name]
+    console.log(goal_info.homepage)
+  }
+}
+
+main()
+*/
+
+const $ = require('jquery')
+require_component('interstitial-screen-block-with-suggestions')
+const {
+  get_minutes_spent_on_domain_today,
+  get_visits_to_domain_today
+} = require('libs_common/time_spent_utils')
+
+const {
+  append_to_body_shadow,
+  once_body_available
+} = require('libs_frontend/frontend_libs')
+
+const {
+  url_to_domain
+} = require('libs_common/domain_utils')
+
+const {
+  get_intervention
+} = require('libs_common/intervention_info');
+
+var shadow_div;
+
+(async function() {
+  /*
+  var domain = url_to_domain(window.location.href)
+  var numMins = await get_minutes_spent_on_domain_today(domain)
+  var numVisits = await get_visits_to_domain_today(domain)
+  var sitename_printable = get_intervention().sitename_printable
+  var buttonText = `Click to continue to ${sitename_printable}`
+  var buttonText2 = `Close ${sitename_printable}`
+  */
+
+  var interst_screen = $('<interstitial-screen-block class="interst_screen">')
+  /*
+  interst_screen.attr('sitename-printable', sitename_printable)
+  interst_screen.attr('intervention', intervention.name)
+  interst_screen.attr('btn-txt', buttonText)
+  interst_screen.attr('btn-txt2', buttonText2)
+  interst_screen.attr('minutes', numMins);
+  interst_screen.attr('visits', numVisits);
+  interst_screen.attr('seconds', 0);
+  */
+
+  once_body_available().then(function() {
+    shadow_div = append_to_body_shadow(interst_screen);
+  })
+})()
+
+window.on_intervention_disabled = () => {
+  $(shadow_div).remove();
+}

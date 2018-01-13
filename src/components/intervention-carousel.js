ca91f9dc1242b6157d1bda71418555f34409af23
estebanreyl <erey@stanford.edu>
2018-01-12 22:17:48 -0800

    Geza's updates, review

--- src/components/intervention-carousel.js
+++ src/components/intervention-carousel.js
@@ -3,33 +3,43 @@ const {polymer_ext} = require('libs_frontend/polymer_utils')
 polymer_ext({
   is: 'intervention-carousel',
   properties: {
+    interventions: {
+      type: Array,
+    }
   },
   ready: async function() {
-      let response = await fetch("");
-      let data = await response.json();
-      let list = data.projects;
-
-      for(let project of list){
-       this.buildProjectCard(project["title"],
-       project["description"],
-       project["imgUrl"],
-       project["date"],
-       project["starCount"]);
-     }
+      // let response = await fetch("");
+      // let data = await response.json();
+      this.interventions = [
+        {
+          name: 'foo'
+        },
+        {
+          name: 'bar'
+        }
+      ]
+      // let list = data.projects;
+      //
+      // for(let project of list){
+      //  this.buildProjectCard(project["title"],
+      //  project["description"],
+      //  project["imgUrl"],
+      //  project["date"],
+      //  project["starCount"]);
    },
-  buildProjectCard: function(title, description, imgUrl, date, starCount){
-    let containerElement = document.querySelector(".container");
-    //Project holder space
-    let card = document.createElement('market-card');
-    card.setAttribute("name", title);
-    card.setAttribute("date", date);
-    card.setAttribute("description", description);
-    card.setAttribute("starCount", starCount);
-    card.addEventListener('click', this.onClick);
-    //Makes sure to insert cards at the front so add button is last
-    containerElement.insert(card);
-  },
-  onClick: function(){
-      //Will Open overlay view with additional info
-  }
+  // buildProjectCard: function(title, description, imgUrl, date, starCount){
+  //   let containerElement = document.querySelector(".container");
+  //   //Project holder space
+  //   let card = document.createElement('market-card');
+  //   card.setAttribute("name", title);
+  //   card.setAttribute("date", date);
+  //   card.setAttribute("description", description);
+  //   card.setAttribute("starCount", starCount);
+  //   card.addEventListener('click', this.onClick);
+  //   //Makes sure to insert cards at the front so add button is last
+  //   containerElement.insert(card);
+  // },
+  // onClick: function(){
+  //     //Will Open overlay view with additional info
+  // }
 })
